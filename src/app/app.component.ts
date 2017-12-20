6a0d9154903bd141db1e3e349cc433766b2a0644
wellwind <wellwind@mail2000.com.tw>
2017-12-20 16:00:18 +0800

    Overlay在按鈕旁邊

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,32 @@ export class AppComponent implements OnInit {
     this.onlineUsers$ = this.onlineUsersService.getOnlineUsers();
   }
 
-  chat(origin: MatButton, user: OnlineUser) {}
+  chat(origin: MatButton, user: OnlineUser) {
+    const strategy = this.overlay
+      .position()
+      .connectedTo(origin._elementRef, { originX: 'end', originY: 'top' }, { overlayX: 'end', overlayY: 'top' })
+      // 備用方案
+      .withFallbackPosition({ originX: 'end', originY: 'bottom' }, { overlayX: 'end', overlayY: 'bottom' });
+
+    const config = new OverlayConfig({
+      hasBackdrop: true,
+      backdropClass: 'cdk-overlay-transparent-backdrop', // 沒有設這個的話, 會有一個灰色背景檔在後面
+      positionStrategy: strategy,
+      scrollStrategy: this.overlay.scrollStrategies.reposition() // scroll後會重新定位
+      // noop: 不做任何事情(預設), close: 關閉, block: 禁止捲動, reposition: 重新定位
+    });
+    const overlayRef = this.overlay.create(config);
+
+    overlayRef.attach(new ComponentPortal(InfoCardComponent, this.viewContainerRef, this._createInjector(overlayRef, user.id)));
+    overlayRef.backdropClick().subscribe(() => overlayRef.detach());
+  }
+
+  private _createInjector(overlayRef: OverlayRef, id: number) {
+    const customTokens = new WeakMap<any, any>();
+    customTokens.set(CHAT_OVERLAY_REF, overlayRef);
+    customTokens.set(CHAT_USER_ID_TOKEN, id);
+    return new PortalInjector(this.viewContainerRef.injector, customTokens);
+  }
 
   chatDialog(origin: MatButton, user: OnlineUser) {}
 }
